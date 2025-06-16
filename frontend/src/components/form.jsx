d1394a1c165d67456d753a41a272c74c682abad6
angelLopezITECAM <angel.lopez@itecam.com>
2025-06-16 08:49:30 +0200

    Arreglo del fallo del btn submit, agregados span a los textos

--- frontend/src/components/form.jsx
+++ frontend/src/components/form.jsx
@@ -221,6 +221,7 @@ export function FormChampi() {
             setOriginalImage(null);
             setAnnotations([]);
 
+
         } catch (error) {
             console.error('Error al enviar el formulario:', error);
             alert(`Error al enviar el formulario: ${error.message}`);
@@ -233,6 +234,7 @@ export function FormChampi() {
     const salasArray = useMemo(() => Array.from({ length: 14 }, (_, i) => ({
         value: (i + 1).toString(), label: `Sala ${i + 1}`,
     })), []);
+
     const muestrasArray = useMemo(() => Array.from({ length: 3 }, (_, i) => ({
         value: (i + 1).toString(), label: `Muestra ${i + 1}`,
     })), []);
@@ -319,15 +321,15 @@ export function FormChampi() {
 
                     <Button type="submit" disabled={isSubmitting}>
                         {isSubmitting ? (
-                            <>
+                            <span className="flex items-center justify-center">
                                 <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                                     <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                                     <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                                 </svg>
                                 Guardando...
-                            </>
+                            </span>
                         ) : (
-                            "Guardar registro"
+                            <span>Guardar registro</span>
                         )}
                     </Button>
                 </form>
