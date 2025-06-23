59af18b7328c2be9b157559fc4e951886fb71b92
angelLopezITECAM <angel.lopez@itecam.com>
2025-06-23 11:08:06 +0200

    Filtro por salas

--- frontend/src/components/history/history-list.jsx
+++ frontend/src/components/history/history-list.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import { API_URL_BASE } from '@/lib/utils'
 import { fetcher } from '@/lib/fetcher';
@@ -7,6 +7,18 @@ import useSWR from "swr"
 export function HistoryList() {
 
     const { data, error, isLoading } = useSWR(`${API_URL_BASE}/images`, fetcher)
+    const [selectedSala, setSelectedSala] = useState(null);
+
+
+    const salasArray = useMemo(() => {
+        if (!data) return [];
+        const salasUnicas = [...new Set(data.map(record => record.sala))];
+        salasUnicas.sort((a, b) => a - b);
+        return salasUnicas.map(sala => ({
+            value: sala.toString(),
+            label: `Sala ${sala}`
+        }));
+    }, [data]);
 
     if (error) return <div>Se ha producido un error cargando los registros</div>
     if (isLoading) return <div>Cargando registros...</div>
@@ -14,12 +26,40 @@ export function HistoryList() {
 
     const dataSorted = data.sort((a, b) => new Date(b.fecha || 0) - new Date(a.fecha || 0));
 
+    const filteredData = selectedSala
+        ? dataSorted.filter(record => record.sala.toString() === selectedSala)
+        : dataSorted;
+
+
 
     return (
         <>
             <h2 className="text-2xl font-semibold mb-4">Historial de anotaciones</h2>
+
+            <h4 className='text-sm font-semibold'>Filtro por sala</h4>
+            <div className='flex flex-row max-w-full overflow-auto gap-4 mb-6 text-sm'>
+
+                <span
+                    key={"all-salas"}
+                    className="block bg-gray-100 hover:bg-gray-200 text-gray-800 font-semibold text-center rounded-lg shadow p-4 transition-colors"
+                    onClick={() => setSelectedSala(selectedSala ? null : "all")}
+                >
+                    Todas las salas
+                </span>
+
+                {salasArray.map((sala) => (
+                    <span
+                        key={sala.value}
+                        className="block bg-gray-100 hover:bg-gray-200 text-gray-800 font-semibold text-center rounded-lg shadow p-4 transition-colors"
+                        onClick={() => setSelectedSala(sala.value === selectedSala ? null : sala.value)}
+                    >
+                        {sala.label}
+                    </span>
+                ))}
+
+            </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                {dataSorted.map((record) => (
+                {filteredData.map((record) => (
                     <Link
                         key={record.id}
                         to={`/historial/${record.id}`}
