98e2bdbda4bea248003c20d9f1947cb3f39497d6
EnriqueJavierVillarCeaItecam <enrique.villar@itecam.com>
2025-07-24 10:39:15 +0200

    Update utils.ts

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,6 +4,6 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
- let API_URL_BASE = `http://192.168.15.12:8001`
+
 /* export const API_URL_BASE = `http://172.20.10.4:8000` */
 export const API_URL_BASE = `http://31.97.123.208:8000`
