b1c5c166985933aa0af38803a4cc1ff6daae6e8e
EnriqueJavierVillarCeaItecam <enrique.villar@itecam.com>
2025-07-24 10:34:27 +0200

    Update utils.ts

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -7,4 +7,4 @@ export function cn(...inputs: ClassValue[]) {
 
 /* export const API_URL_BASE = `http://192.168.15.12:8001` */
 /* export const API_URL_BASE = `http://172.20.10.4:8000` */
-export const API_URL_BASE = `http://localhost:8000`
+export const API_URL_BASE = `http://31.97.123.208:8000`
